fc3be01b879576f1a6d3c527cf1298e825dd4542
Colégio Concórdia <vs2020concordia@hotmail.com>
2023-09-29 17:00:05 -0300

    ADD Sistema Hoteleiro

---
+++ Sistema Hoteleiro/Hotel-CLC/Login.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Hotel_CLC
+{
+    partial class frLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frLogin));
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnlLogin.BackColor = System.Drawing.Color.Transparent;
+            this.pnlLogin.BackgroundImage = global::Hotel_CLC.Properties.Resources.LOGIN__2_;
+            this.pnlLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pnlLogin.Location = new System.Drawing.Point(132, 63);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(320, 224);
+            this.pnlLogin.TabIndex = 0;
+            // 
+            // frLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 371);
+            this.Controls.Add(this.pnlLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LOGIN";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frLogin_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLogin;
+    }
+}
+
---
+++ Sistema Hoteleiro/Hotel-CLC/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel_CLC
+{
+    public partial class frLogin : Form
+    {
+        public frLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frLogin_Load(object sender, EventArgs e)
+        {
+            //pnlLogin.Location = new Point(0, 0);
+            //pnlLogin.Location = new Point(this.Width / 2, this.Height / 2);
+            //pnlLogin.Location = new Point(this.Width - 160, this.Height - 112);
+            //pnlLogin.Location = new Point(this.Width / 2 - pnlLogin.BackgroundImage.Size.Width, this.Height / 2 - pnlLogin.BackgroundImage.Size.Height);
+            //pnlLogin.Location = new Point(this.Width / 2 - pnlLogin.BackgroundImage.Width, this.Height / 2 - pnlLogin.BackgroundImage.Height);
+        }
+    }
+}
